674d49038faeead250ef2c88100826b238aa97ca
Phongsathon  Fukphon <125950212+Sixsax12@users.noreply.github.com>
2025-12-18 18:01:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -91,6 +91,5 @@ def show_history():
 def download_file(filename):
     return send_from_directory('static', filename, as_attachment=True)
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
